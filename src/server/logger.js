a0da43f43cf5633ddc2a0b46d008646990459e87
Shaik Sadath Basha <sadath.sa@gmail.com>
2020-12-23 15:25:40 +0530

    Added colors to the info and error in logger.js

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,14 +1,38 @@
 const winston = require('winston');
 
+const logColors = {
+	colors: {
+		info: 'cyan',
+		error: 'red',
+	},
+};
+
+winston.addColors(logColors);
+
 const logger = winston.createLogger({
 	transports: [
 		new winston.transports.Console({
 			level: 'info',
-			format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.timestamp(),
+				winston.format.json(),
+				winston.format.printf(
+					(info) => `{level: ${info.level}, message: ${info.message}, timestamp: ${info.timestamp}}`
+				)
+			),
 		}),
 		new winston.transports.Console({
 			level: 'error',
-			format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.timestamp(),
+				winston.format.json(),
+				winston.format.printf(
+					(error) =>
+						`{level: ${error.level}, message: ${error.message}, timestamp: ${error.timestamp}}`
+				)
+			),
 		}),
 		new winston.transports.File({
 			level: 'info',
